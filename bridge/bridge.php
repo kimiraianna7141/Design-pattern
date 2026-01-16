e7f9c0a15a7f7bb84717c155bcfbbab17ede0213
kimi <kimirainnam12@gmail.com>
2026-01-16 09:43:17 +0100

    added the bridge design pattern

---
+++ bridge/bridge.php
@@ -0,0 +1,10 @@
+<?php
+abstract class Shape {
+    protected Color $color;
+
+    public function __construct(Color $color) {
+        $this->color = $color;
+    }
+
+    abstract public function draw();
+}
