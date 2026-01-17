f688e351290cff7ef4fd41f3890764c807fbea9a
kimi <kimirainnam12@gmail.com>
2026-01-17 05:31:52 +0100

    added state design patterns

--- facade/STATEDESIGNPATTERNS/states.php
+++
@@ -1,8 +0,0 @@
-<?php
-abstract class Shape {
-    protected Color $color;
-
-    public function __construct(Color $color) {
-        $this->color = $color;
-    }
-} 
---
+++ state.php
@@ -0,0 +1,18 @@
+<?php
+class ComputerFacade{
+    private $cpu;
+    private $memory;
+    private $hardDrive;
+    
+    public function_construct(){
+        $this->cpu = new CPU();
+        $this->hardDrive = new HardDrive();
+    }
+    
+        public function startComputer() {
+            $this->cpu->freeze();
+            $this->hardDrive->read();
+            $this->memory->load();
+            $this->cpu->execute();
+        }
+    }
